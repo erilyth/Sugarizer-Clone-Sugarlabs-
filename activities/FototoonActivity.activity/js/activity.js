65d1c51ff799a3e0509a438805125eec35dc8c7e
erilyth <vishal.batchu@students.iiit.ac.in>
2016-03-05 12:23:58 +0530

    Add basic MVC architecture

--- activities/FototoonActivity.activity/js/activity.js
+++ activities/FototoonActivity.activity/js/activity.js
@@ -1,11 +1,26 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
+    var datastore = require("sugar-web/datastore");
+
+    var model = require("activity/model");
+    var view = require("activity/view");
+    var controller = require("activity/controller");
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
 
+    	var speak;
+
         // Initialize the activity.
         activity.setup();
+
+        function Speak() {
+            this.model = new model.Model();
+            this.view = new view.View();
+            this.controller = new controller.Controller(this.model, this.view);
+        }
+
+        speak = new Speak();
 
         document.getElementById('test').innerHTML = "This is the fototoon activity";
 
---
+++ activities/FototoonActivity.activity/js/model.js
@@ -0,0 +1,11 @@
+define(function (require) {
+
+    var model = {};
+
+    model.Model = function () {
+        this.test = "This is the model";
+    };
+
+    return model;
+
+});
